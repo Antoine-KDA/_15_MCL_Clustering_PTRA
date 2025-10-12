045171aa3351d389cdfefbb6ea725dce0e91117c
Antoine-KDA <antoine.kossi@Gmail.com>
2025-10-12 15:14:30 +0200

    include all patient history plus added death data

--- Scripts_clustering_ptra/_02_ptra_reduce_data.R
+++ Scripts_clustering_ptra/_02_ptra_reduce_data.R
@@ -18,28 +18,62 @@
 #' 3- Create a treatments data (in csv)
 # 1- Load the inpatient and outpatient data
 
-source("Scripts/_01_Create_reduced_index_data.R", echo = TRUE)
+# source("Scripts/_01_Create_reduced_index_data.R", echo = TRUE)
 
+# 1- Import the index data ------------------------------------------------------------
+
+filpath <- "../00_Original_Data_Antoine/raw_data"
+
+# load the index data 
+index <- rio::import(file.path(filpath, "index_pop.csv")); dim(index)
+# Load the inpatient data
+inpatient <- rio::import(file.path(filpath, "spr_fk.csv"))
+# Load the outpatient data
+outpatient <- rio::import(file.path(filpath, "opr_fk.csv"))
+
+
+# 1.1- Identify and remove duplicated patients in the index data (point 1)
+
+index %>% 
+  group_by(lopnr) %>% 
+  filter(n() > 1) %>% 
+  ungroup() -> dup_patients
+# dim(dup_patients)
+
+# 1.2- Remove duplicated: if case == 1, remove entire riskset of the corresponding case, if case == 0, remove only the lopnr
+
+if (nrow(dup_patients) > 0) {
+  index %>% 
+    anti_join(dup_patients %>% filter(case == 1), by = "riskset") -> index
+  index %>% 
+    anti_join(dup_patients %>% filter(case == 0), by = "lopnr") -> index
+}
+# dim(index)
+# [1] 22561    54
 
 # 2.1- Get the necessary columns from the index data ------------------
 patients <- index %>%
-  select(lopnr, birth_dt, female) %>%
+  select(lopnr, birth_dt, female, death_dt) %>%
   distinct() %>%
   rename(patient_id = lopnr) %>%
   mutate(birth_year = lubridate::year(birth_dt),
+         birth_dt = as.Date(birth_dt),
+         death_dt = as.Date(death_dt),
+         year_month_death = ifelse(is.na(death_dt), "202312", format(death_dt, "%Y%m")),
+         age_at_death = as.integer((death_dt - birth_dt)/365.25),
          sex = ifelse(female == 1, "F", "M"),
          col1 = paste0("\\\\000"), col2 = paste0("\\\\000"), 
          col3 = paste0("\\\\000"), col4 = paste0("\\\\000"), col5 = paste0("\\\\000"),
          col6 = paste0("\\\\000"), col7 = paste0("\\\\000"), col8 = paste0("\\\\000"),
          col9 = paste0("\\\\000")) %>%
-  select(patient_id, sex, col1, col2, birth_year, col3, col4, col5, col6, col7, col8, col9)
+  select(patient_id, sex, col1, col2, birth_year, age_at_death, col4, col5, col6, col7, year_month_death, col9)
 
 # patients %>% head(100) %>% View()
 
 # 2.2- Get the necessary columns from the index_inpat and index_outpat data ------------------
 
 # The impatient data
-red_index_inpat <- index_inpat %>%
+red_index_inpat <- inpatient %>%
   select(lopnr, riskset, entry_dt, contains("datuma"), ar, alder,
          contains("dia")) %>% 
   select(-dia_ant) %>%
@@ -78,7 +112,7 @@ red_index_inpat$dx[1:100]
 # head(red_index_inpat, 100) %>% View()
 
 # The outpatient data
-red_index_outpat <- index_outpat %>%  
+red_index_outpat <- outpatient %>%  
   select(lopnr, riskset, entry_dt, contains("datuma"), ar, alder,
          contains("dia")) %>% 
     select(-dia_ant) %>%
@@ -117,7 +151,7 @@ red_index_outpat <- index_outpat %>%
 # Combine inpatient, outpatient, and drug data into one long format
 ptra_diagnosis_data <- bind_rows(red_index_inpat, red_index_outpat) %>%
  # Reformat the date to be in the correct format of DD/MM/YYYY instead of YYYY-MM-DD
-  mutate(dx_dt = format(dx_dt, "%d/%m/%Y")) %>%
+#   mutate(dx_dt = format(dx_dt, "%d/%m/%Y")) %>%
   arrange(patient_id, dx_dt) %>%
   distinct() # Remove duplicates
 
